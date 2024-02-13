072fd8e9d1c89d56b45b392cfbd2d69769f26c1c
M-C-Dani <martincastillodan@gmail.com>
2024-02-13 13:50:24 +0100

    añadir usserName a accounts

--- js-version/script.js
+++ js-version/script.js
@@ -33,6 +33,13 @@ const account4 = {
   pin: 4444,
 };
 
+const account4 = {
+  owner: 'Juan Sánchez',
+  movements: [523, 900, 1200, 20, -90],
+  interestRate: 1,
+  pin: 5555,
+};
+
 const accounts = [account1, account2, account3, account4];
 
 // Elements
@@ -60,3 +67,20 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+//crear  funcion CreateUsername que reciba array de cuentas
+//y devuelva un nuevo array con los nombres de usuarios de cada cuenta
+//en minusculas y sin espacios
+//por ej Juan Sanches -> js
+
+const  createUsernames = functions (accounts) {
+  accounts.forEach(function (account)){
+    sccount.username = account.owner //Juan Sánchez
+    .toLowerCase() //juan sánchez
+    .split('') //['juan sánchez']
+    .map(name=>[0]) //['juan' , 'sánchez']
+    .join('') //js (lo contrario que split)
+    return {...account, username: username}
+  }  
+}
+createUsernames(aacounts)
